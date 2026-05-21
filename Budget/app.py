508cf90fc7a558f4844ff7b93037fb8179000c22
molly-jean99 <mollyjeangilliland@gmail.com>
2026-05-21 19:18:03 -0400

    Update app.py

--- Budget/app.py
+++ Budget/app.py
@@ -75,4 +75,7 @@ def delete(index):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
